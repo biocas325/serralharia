b71650011f44c2e253ee3e0906da9ce5ac062ea6
biocas325 <biaafariaaa0306@gmail.com>
2026-07-21 10:00:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
 
+    if (window.scrollY > 50) {
+        header.style.boxShadow = "0 4px 20px rgba(0,0,0,0.4)";
+    } else {
+        header.style.boxShadow = "none";
+    }
+});
